9f939edc6b5246cb3773eba4f0a22db005d92f39
Jayasurya Pazhani <jsurya733@gmail.com>
2024-02-17 18:24:28 -0500

    Methods to add player to game

--- src/main/Java/Services/PlayerService.java
+++ src/main/Java/Services/PlayerService.java
@@ -3,7 +3,8 @@ package Services;
 import Constants.ApplicationConstants;
 import Models.*;
 import Utils.CommonUtil;
-
+import java.util.Random;
+import Exceptions.InvalidCommand;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,5 +48,34 @@ public class PlayerService {
                 System.out.println("Unrecognized action on the Players list.");
         }
         return l_updatedPlayers;
+    }
+    private void removeGamePlayer(List<Player> p_existingPlayerList, List<Player> p_updatedPlayers, String p_enteredPlayerName, boolean p_playerNameAlreadyExist)
+    {
+        if (p_playerNameAlreadyExist)
+        {
+            for (Player l_player : p_existingPlayerList)
+            {
+                if (l_player.getPlayerName().equalsIgnoreCase(p_enteredPlayerName))
+                {
+                    p_updatedPlayers.remove(l_player);
+                    System.out.println("Player : " + p_enteredPlayerName + " has been successfully removed from the list");
+                }
+            }
+        } else {
+            System.out.print("Player : " + p_enteredPlayerName + " does not Exist. No changes made so far.");
+        }
+    }
+    private void addGamePlayer(List<Player> p_updatedPlayers, String p_enteredPlayerName, boolean p_playerNameAlreadyExist)
+    {
+        if (p_playerNameAlreadyExist)
+        {
+            System.out.print("Player : " + p_enteredPlayerName + " already exists. No changes made.");
+        }
+        else
+        {
+            Player l_addNewPlayer = new Player(p_enteredPlayerName);
+            p_updatedPlayers.add(l_addNewPlayer);
+            System.out.println("Player : " + p_enteredPlayerName + " has been added to the list successfully.");
+        }
     }
 }
