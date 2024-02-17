61a622082b6467c8815496ea2d3465511bcd81b2
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2024-02-16 21:02:27 -0500

    Color Codes and Colors Available

--- src/main/Java/Constants/ApplicationConstants.java
+++ src/main/Java/Constants/ApplicationConstants.java
@@ -32,4 +32,17 @@ public final class ApplicationConstants {
     public static final String SRC_MAIN_RESOURCES = "src/main/resources";
     public static final int CONSOLE_WIDTH = 80;
 
+    // ANSI- Color Codes
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\u001B[47m";
+
+    // Colors Available
+    public static final List<String> COLORS = Arrays.asList(RED, GREEN, YELLOW, BLUE, PURPLE, CYAN);
+
+
 }
