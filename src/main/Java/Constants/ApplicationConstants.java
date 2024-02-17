02ce8fd7337f7dd9bf1f6cfb150f17713c54b7bb
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2024-02-16 20:02:42 -0500

    Invalid Commands and Valid Map Constants

---
+++ src/main/Java/Constants/ApplicationConstants.java
@@ -0,0 +1,21 @@
+package Constants;
+import java.util.Arrays;
+import java.util.List;
+
+public final class ApplicationConstants {
+    // Error messages for invalid commands
+    public static final String INVALID_COMMAND_ERROR_EDITMAP = "Invalid command. Please provide the command in the following format: editmap filename";
+    public static final String INVALID_COMMAND_ERROR_EDITCONTINENT = "Invalid command. Please provide the command in the following format: editcontinent -add continentID continentvalue -remove continentID";
+    public static final String INVALID_COMMAND_ERROR_EDITCOUNTRY = "Invalid command. Please provide the command in the following format: editcountry -add countryID continentID -remove countryID";
+    public static final String INVALID_COMMAND_ERROR_EDITNEIGHBOUR = "Invalid command. Please provide the command in the following format: editneighbor -add countryID neighborcountryID -remove countryID neighborcountryID";
+    public static final String INVALID_COMMAND_ERROR_SAVEMAP = "Invalid command. Please provide the command in the following format: savemap filename";
+    public static final String INVALID_MAP_ERROR_EMPTY = "No map found! Please load a valid map to check.";
+    public static final String INVALID_COMMAND_ERROR_LOADMAP = "Invalid command. Please provide the command in the following format: loadmap filename";
+    public static final String INVALID_COMMAND_ERROR_VALIDATEMAP = "Invalid command! Validatemap is not supposed to have any arguments.";
+    public static final String INVALID_COMMAND_ERROR_GAMEPLAYER = "Invalid command. Please provide the command in the following format: gameplayer -add playername -remove playername";
+    public static final String INVALID_MAP_LOADED = "Map cannot be loaded as it is invalid. Please provide a valid map.";
+    public static final String INVALID_COMMAND_ERROR_ASSIGNCOUNTRIES = "Invalid command. Please provide the command in the following format: assigncountries";
+    public static final String INVALID_COMMAND_ERROR_DEPLOY_ORDER = "Invalid command. Please provide the command in the following format: deploy countryID <CountryName> <num> (until all reinforcements have been placed).";
+    public static final String VALID_MAP = "The loaded map is valid!";
+
+}
