5ff844b36fc8fccc2a30e297c7cc01ff51dd2725
SHAHULPTS <s_thamee@mail.concordia.ca>
2024-02-17 16:58:06 -0500

    Created order class and declared variable

---
+++ src/main/Java/Models/Order.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class Order {
+    String d_orderAction;
+    String d_targetCountryName;
+    String d_sourceCountryName;
+    Integer d_numberOfArmiesToPlace;
+    Order orderObj;
+}
