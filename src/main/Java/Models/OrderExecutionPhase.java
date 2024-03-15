9c2347bf37d240adcfc6331436ea144fe1ea1eed
SHAHULPTS <s_thamee@mail.concordia.ca>
2024-03-15 19:58:01 -0400

    OrderExecutionPhase file created

---
+++ src/main/Java/Models/OrderExecutionPhase.java
@@ -0,0 +1,135 @@
+package Models;
+
+import Controllers.GameEngine;
+import Exceptions.InvalidCommand;
+import Exceptions.InvalidMap;
+import Utils.Command;
+import Utils.CommonUtil;
+import Views.MapView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class OrderExecutionPhase extends Phase {
+
+
+    public OrderExecutionPhase(GameEngine p_gameEngine, GameState p_gameState) {
+        super(p_gameEngine, p_gameState);
+    }
+
+    @Override
+    protected void performCardHandle(String p_enteredCommand, Player p_player) throws IOException {
+        printInvalidCommandInState();
+    }
+
+    @Override
+    protected void performAdvance(String p_command, Player p_player) {
+        printInvalidCommandInState();
+    }
+
+    @Override
+    public void initPhase() {
+        while (d_gameEngine.getD_CurrentPhase() instanceof OrderExecutionPhase) {
+            executeOrders();
+
+            MapView l_map_view = new MapView(d_gameState);
+            l_map_view.showMap();
+
+            if (this.checkEndOftheGame(d_gameState))
+                break;
+
+            while (!CommonUtil.isCollectionEmpty(d_gameState.getD_players())) {
+                System.out.println("Press Y/y if you want to continue for next turn or else press N/n");
+                BufferedReader l_reader = new BufferedReader(new InputStreamReader(System.in));
+
+                try {
+                    String l_continue = l_reader.readLine();
+
+                    if (l_continue.equalsIgnoreCase("N")) {
+                        break;
+                    } else if(l_continue.equalsIgnoreCase("Y")){
+                        d_playerService.assignArmies(d_gameState);
+                        d_gameEngine.setIssueOrderPhase();
+                    } else {
+                        System.out.println("Invalid Input");
+                    }
+                } catch (IOException l_e) {
+                    System.out.println("Invalid Input");
+                }
+            }
+        }
+    }
+
+
+    protected void executeOrders() {
+        addNeutralPlayer(d_gameState);
+        // Executing orders
+        d_gameEngine.setD_gameEngineLog("\nStarting Execution Of Orders.....", "start");
+        while (d_playerService.unexecutedOrdersExists(d_gameState.getD_players())) {
+            for (Player l_player : d_gameState.getD_players()) {
+                Order l_order = l_player.next_order();
+                if (l_order != null) {
+                    l_order.printOrder();
+                    d_gameState.updateLog(l_order.orderExecutionLog(), "effect");
+                    l_order.execute(d_gameState);
+                }
+            }
+        }
+        d_playerService.resetPlayersFlag(d_gameState.getD_players());
+    }
+
+
+    public void addNeutralPlayer(GameState p_gameState) {
+        Player l_player = p_gameState.getD_players().stream()
+                .filter(l_pl -> l_pl.getPlayerName().equalsIgnoreCase("Neutral")).findFirst().orElse(null);
+        if (CommonUtil.isNull(l_player)) {
+            Player l_neutralPlayer = new Player("Neutral");
+            l_neutralPlayer.setD_moreOrders(false);
+            p_gameState.getD_players().add(l_neutralPlayer);
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    protected void performShowMap(Command p_command, Player p_player) {
+        MapView l_mapView = new MapView(d_gameState);
+        l_mapView.showMap();
+    }
+
+    @Override
+    protected void performCreateDeploy(String p_command, Player p_player) {
+        printInvalidCommandInState();
+    }
+
+
+    @Override
+    protected void performAssignCountries(Command p_command, Player p_player) throws InvalidCommand, IOException {
+        printInvalidCommandInState();
+    }
+
+
+    @Override
+    protected void createPlayers(Command p_command, Player p_player) throws InvalidCommand {
+        printInvalidCommandInState();
+    }
+
+
+    @Override
+    protected void performEditNeighbour(Command p_command, Player p_player)
+            throws InvalidCommand, InvalidMap, IOException {
+        printInvalidCommandInState();
+    }
+
+
+    @Override
+    protected void performEditCountry(Command p_command, Player p_player)
+            throws InvalidCommand, InvalidMap, IOException {
+        printInvalidCommandInState();
+    }
+
+
+
+}
