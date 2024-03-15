33628752b7c37c6348dc5edb051e08aeb9a04f58
Shuvanidhi <shuvanidhi21@gmail.com>
2024-03-15 19:34:32 -0400

    Updated java doc comments in LogWriter and LogEntryBuffer java files
    Updated java doc comments in LogWriter and LogEntryBuffer java files

--- src/main/Java/Views/LogWriter.java
+++ src/main/Java/Views/LogWriter.java
@@ -10,10 +10,21 @@ import java.nio.file.StandardOpenOption;
 import java.util.Observable;
 import java.util.Observer;
 
+/**
+ * The LogWriter class implements the Observer interface to observe changes in the LogEntryBuffer.
+ * It writes log messages to a file.
+ */
 public class LogWriter implements Observer {
 
+    /** The LogEntryBuffer instance to observe. */
     private LogEntryBuffer d_logEntryBuffer;
 
+    /**
+     * This method is called whenever the observed object (LogEntryBuffer) is changed.
+     *
+     * @param p_observable the Observable object being observed, expected to be a LogEntryBuffer instance
+     * @param p_object     the argument passed to the notifyObservers method, representing the changed object
+     */
     @Override
     public void update(Observable p_observable, Object p_object) {
         if (!(p_object instanceof LogEntryBuffer)) {
