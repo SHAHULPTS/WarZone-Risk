00be5e5b45aba688ff3b518273368bf4c728db5a
Shuvanidhi <shuvanidhi21@gmail.com>
2024-03-15 14:41:03 -0400

    Added Blockade.java and Bomb.java files
    Added Blockade.java and Bomb.java files

---
+++ src/main/Java/Models/Blockade.java
@@ -0,0 +1,103 @@
+package Models;
+
+import Utils.CommonUtil;
+
+public class Blockade implements Card {
+
+    Player d_playerInitiator;
+
+    String d_targetCountryID;
+
+    String d_orderExecutionLog;
+
+    public Blockade(Player p_playerInitiator, String p_targetCountry) {
+        this.d_playerInitiator = p_playerInitiator;
+        this.d_targetCountryID = p_targetCountry;
+    }
+
+    @Override
+    public void execute(GameState p_gameState) {
+        if (valid(p_gameState)) {
+            Country l_targetCountryID = p_gameState.getD_map().getCountryByName(d_targetCountryID);
+            Integer l_noOfArmiesOnTargetCountry = l_targetCountryID.getD_armies() == 0 ? 1
+                    : l_targetCountryID.getD_armies();
+            l_targetCountryID.setD_armies(l_noOfArmiesOnTargetCountry * 3);
+
+            d_playerInitiator.getD_coutriesOwned().remove(l_targetCountryID);
+
+            Player l_player = p_gameState.getD_players().stream()
+                    .filter(l_pl -> l_pl.getPlayerName().equalsIgnoreCase("Neutral")).findFirst().orElse(null);
+
+            if (!CommonUtil.isNull(l_player)) {
+                l_player.getD_coutriesOwned().add(l_targetCountryID);
+                System.out.println("Neutral territory: " + l_targetCountryID.getD_countryName() + "assigned to the Neutral Player.");
+            }
+
+            d_playerInitiator.removeCard("blockade");
+            this.setD_orderExecutionLog("\nPlayer : " + this.d_playerInitiator.getPlayerName()
+                    + " is executing defensive blockade on Country :  " + l_targetCountryID.getD_countryName()
+                    + " with armies :  " + l_targetCountryID.getD_armies(), "default");
+            p_gameState.updateLog(orderExecutionLog(), "effect");
+        }
+    }
+
+    @Override
+    public boolean valid(GameState p_gameState) {
+
+        Country l_country = d_playerInitiator.getD_coutriesOwned().stream()
+                .filter(l_pl -> l_pl.getD_countryName().equalsIgnoreCase(this.d_targetCountryID)).findFirst()
+                .orElse(null);
+
+        if (CommonUtil.isNull(l_country)) {
+            this.setD_orderExecutionLog(this.currentOrder() + " is not executed since Target country : "
+                    + this.d_targetCountryID + " given in blockade command does not owned to the player : "
+                    + d_playerInitiator.getPlayerName()
+                    + " The card will have no affect and you don't get the card back.", "error");
+            p_gameState.updateLog(orderExecutionLog(), "effect");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void printOrder() {
+        this.d_orderExecutionLog = "----------Blockade card order issued by player "
+                + this.d_playerInitiator.getPlayerName() + "----------" + System.lineSeparator()
+                + "Creating a defensive blockade with armies = " + "on country ID: " + this.d_targetCountryID;
+        System.out.println(System.lineSeparator() + this.d_orderExecutionLog);
+
+    }
+
+    public void setD_orderExecutionLog(String p_orderExecutionLog, String p_logType) {
+        this.d_orderExecutionLog = p_orderExecutionLog;
+        if (p_logType.equals("error")) {
+            System.err.println(p_orderExecutionLog);
+        } else {
+            System.out.println(p_orderExecutionLog);
+        }
+    }
+
+    @Override
+    public Boolean checkValidOrder(GameState p_gameState) {
+        Country l_targetCountry = p_gameState.getD_map().getCountryByName(d_targetCountryID);
+        if (l_targetCountry == null) {
+            this.setD_orderExecutionLog("Invalid Target Country! Doesn't exist on the map!", "error");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String getOrderName() {
+        return "blockade";
+    }
+
+    private String currentOrder() {
+        return "Blockade card order : " + "blockade" + " " + this.d_targetCountryID;
+    }
+
+    public String orderExecutionLog() {
+        return this.d_orderExecutionLog;
+    }
+
+}
