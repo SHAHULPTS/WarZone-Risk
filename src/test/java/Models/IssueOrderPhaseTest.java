73baa5373e4a914f2d14e17fdfc596ba8e6f1a16
Jayanth Apagundi <42775370+JayanthApagundi@users.noreply.github.com>
2024-03-16 14:53:57 -0400

    Test Files of Negotiation and Player added

---
+++ src/test/java/Models/IssueOrderPhaseTest.java
@@ -0,0 +1,82 @@
+package Models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IssueOrderPhaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void performCardHandle() {
+    }
+
+    @Test
+    void performShowMap() {
+    }
+
+    @Test
+    void performAdvance() {
+    }
+
+    @Test
+    void initPhase() {
+    }
+
+    @Test
+    void performCreateDeploy() {
+    }
+
+    @Test
+    void issueOrders() {
+    }
+
+    @Test
+    void askForOrder() {
+    }
+
+    @Test
+    void performAssignCountries() {
+    }
+
+    @Test
+    void createPlayers() {
+    }
+
+    @Test
+    void performEditNeighbour() {
+    }
+
+    @Test
+    void performEditCountry() {
+    }
+
+    @Test
+    void performValidateMap() {
+    }
+
+    @Test
+    void performLoadMap() {
+    }
+
+    @Test
+    void performSaveMap() {
+    }
+
+    @Test
+    void performEditContinent() {
+    }
+
+    @Test
+    void performMapEdit() {
+    }
+}
