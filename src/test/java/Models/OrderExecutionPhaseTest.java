173a91f3c33b6b29de221f52f99f66fe8aa5d56f
SHAHULPTS <s_thamee@mail.concordia.ca>
2024-03-15 20:40:52 -0400

    Created @Before method to to OrderExecutionPhaseTest file

--- src/test/java/Models/OrderExecutionPhaseTest.java
+++ src/test/java/Models/OrderExecutionPhaseTest.java
@@ -12,5 +12,41 @@ import org.junit.Test;
 import Controllers.GameEngine;
 import Exceptions.InvalidMap;
 public class OrderExecutionPhaseTest {
+    Player d_player1;
+
+
+    Player d_player2;
+
+
+    GameState d_gameState;
+
+
+    @Before
+    public void setup() throws InvalidMap {
+        d_gameState = new GameState();
+        d_player1 = new Player();
+        d_player1.setPlayerName("a");
+        d_player2 = new Player();
+        d_player2.setPlayerName("b");
+
+        List<Country> l_countryList = new ArrayList<Country>();
+        Country l_country = new Country(0, "France", 1);
+        l_country.setD_armies(9);
+        l_countryList.add(l_country);
+
+        Country l_countryNeighbour = new Country(1, "Belgium", 1);
+        l_countryNeighbour.addNeighbour(0);
+        l_country.addNeighbour(1);
+        l_countryNeighbour.setD_armies(10);
+        l_countryList.add(l_countryNeighbour);
+
+        d_player1.setD_coutriesOwned(l_countryList);
+
+        Map l_map = new Map();
+        l_map.setD_countries(l_countryList);
+        d_gameState.setD_map(l_map);
+        d_gameState.setD_players(Arrays.asList(d_player1, d_player2));
+    }
+
 
 }
