42925fa372409e002faf3c1b8eefdbdac23519f4
SHAHULPTS <s_thamee@mail.concordia.ca>
2024-03-15 20:28:08 -0400

    Added javadoc comments to OrderExecutionPhase file

--- src/main/Java/Models/OrderExecutionPhase.java
+++ src/main/Java/Models/OrderExecutionPhase.java
@@ -11,24 +11,48 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
+/**
+ * Represents the phase where orders are executed in the game.
+ * Extends Phase class.
+ */
 public class OrderExecutionPhase extends Phase {
 
-
+    /**
+     * Constructs an OrderExecutionPhase object with the specified game engine and game state.
+     *
+     * @param p_gameEngine The game engine associated with this phase.
+     * @param p_gameState  The game state associated with this phase.
+     */
     public OrderExecutionPhase(GameEngine p_gameEngine, GameState p_gameState) {
         super(p_gameEngine, p_gameState);
     }
 
+    /**
+     * Performs card handling operation.
+     *
+     * @param p_enteredCommand The entered command.
+     * @param p_player         The player who entered the command.
+     * @throws IOException If an I/O error occurs.
+     */
     @Override
     protected void performCardHandle(String p_enteredCommand, Player p_player) throws IOException {
         printInvalidCommandInState();
     }
+    /**
+     * Performs advance operation.
+     *
+     * @param p_command The command entered by the player.
+     * @param p_player  The player who entered the command.
+     */
 
     @Override
     protected void performAdvance(String p_command, Player p_player) {
         printInvalidCommandInState();
     }
 
+    /**
+     * Initializes the order execution phase.
+     */
     @Override
     public void initPhase() {
         while (d_gameEngine.getD_CurrentPhase() instanceof OrderExecutionPhase) {
@@ -63,6 +87,9 @@ public class OrderExecutionPhase extends Phase {
     }
 
 
+    /**
+     * Executes the orders of the players.
+     */
     protected void executeOrders() {
         addNeutralPlayer(d_gameState);
         // Executing orders
@@ -80,7 +107,11 @@ public class OrderExecutionPhase extends Phase {
         d_playerService.resetPlayersFlag(d_gameState.getD_players());
     }
 
-
+    /**
+     * Adds a neutral player if not already present in the game state.
+     *
+     * @param p_gameState The game state where the neutral player needs to be added.
+     */
     public void addNeutralPlayer(GameState p_gameState) {
         Player l_player = p_gameState.getD_players().stream()
                 .filter(l_pl -> l_pl.getPlayerName().equalsIgnoreCase("Neutral")).findFirst().orElse(null);
@@ -93,73 +124,103 @@ public class OrderExecutionPhase extends Phase {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performShowMap(Command p_command, Player p_player) {
         MapView l_mapView = new MapView(d_gameState);
         l_mapView.showMap();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performCreateDeploy(String p_command, Player p_player) {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performAssignCountries(Command p_command, Player p_player) throws InvalidCommand, IOException {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void createPlayers(Command p_command, Player p_player) throws InvalidCommand {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performEditNeighbour(Command p_command, Player p_player)
             throws InvalidCommand, InvalidMap, IOException {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performEditCountry(Command p_command, Player p_player)
             throws InvalidCommand, InvalidMap, IOException {
         printInvalidCommandInState();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     protected void performValidateMap(Command p_command, Player p_player) throws InvalidMap, InvalidCommand {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performLoadMap(Command p_command, Player p_player) throws InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performSaveMap(Command p_command, Player p_player) throws InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performEditContinent(Command p_command, Player p_player)
             throws IOException, InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void performMapEdit(Command p_command, Player p_player) throws IOException, InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-
+    /**
+     * Checks if the game has reached its end condition.
+     *
+     * @param p_gameState The game state to check.
+     * @return True if the game has ended, false otherwise.
+     */
     protected Boolean checkEndOftheGame(GameState p_gameState) {
         Integer l_totalCountries = p_gameState.getD_map().getD_countries().size();
         for (Player l_player : p_gameState.getD_players()) {
@@ -169,7 +230,6 @@ public class OrderExecutionPhase extends Phase {
                 return true;
             }
         }
-
         return false;
     }
 
