a5899c9a88a978055a2c4c2ad8a224c4b3cba0f3
Shuvanidhi <shuvanidhi21@gmail.com>
2024-04-06 17:42:50 -0400

    Added java doc comments
    Added java doc comments in AgressivePlayer, BenevolentPlayer and CheaterPlayer java files

--- src/main/Java/Models/AggressivePlayer.java
+++ src/main/Java/Models/AggressivePlayer.java
@@ -7,10 +7,22 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Random;
 
+/**
+ * AggressivePlayer class represents a player behavior strategy that
+ * focuses on aggressive gameplay.
+ */
 public class AggressivePlayer extends PlayerBehaviorStrategy {
 
+    /** List of countries to deploy armies */
     ArrayList<Country> d_deployCountries = new ArrayList<Country>();
 
+    /**
+     * Creates an order for the player based on aggressive strategy.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created order as a string.
+     */
     @Override
     public String createOrder(Player p_player, GameState p_gameState) {
         System.out.println("Creating order for : " + p_player.getPlayerName());
@@ -64,6 +76,13 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
         return l_command;
     }
 
+    /**
+     * Creates a deploy order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created deploy order as a string.
+     */
     @Override
     public String createDeployOrder(Player p_player, GameState p_gameState) {
         Random l_random = new Random();
@@ -76,6 +95,13 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
         return String.format("deploy %s %d", l_strongestCountry.getD_countryName(), l_armiesToDeploy);
     }
 
+    /**
+     * Creates an advance order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created advance order as a string.
+     */
     @Override
     public String createAdvanceOrder(Player p_player, GameState p_gameState) {
         Country l_randomSourceCountry = getRandomCountry(d_deployCountries);
@@ -93,6 +119,13 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
 
     }
 
+    /**
+     * Moves armies from neighbors of a given country to the country.
+     *
+     * @param p_player The player owning the country.
+     * @param p_randomSourceCountry The country from which to move armies.
+     * @param p_gameState The current game state.
+     */
     public void moveArmiesFromItsNeighbors(Player p_player, Country p_randomSourceCountry, GameState p_gameState) {
         List<Integer> l_adjacentCountryIds = p_randomSourceCountry.getD_adjacentCountryIds();
         List<Country> l_listOfNeighbors = new ArrayList<Country>();
@@ -114,11 +147,25 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
         p_randomSourceCountry.setD_armies(l_ArmiesToMove);
     }
 
+    /**
+     * Retrieves a random country from a list of countries.
+     *
+     * @param p_listOfCountries The list of countries to choose from.
+     * @return A randomly selected country.
+     */
     private Country getRandomCountry(List<Country> p_listOfCountries) {
         Random l_random = new Random();
         return p_listOfCountries.get(l_random.nextInt(p_listOfCountries.size()));
     }
 
+    /**
+     * Creates a card order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @param p_cardName The name of the card to be used.
+     * @return The created card order as a string.
+     */
     @Override
     public String createCardOrder(Player p_player, GameState p_gameState, String p_cardName) {
         Random l_random = new Random();
@@ -144,6 +191,13 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
         return null;
     }
 
+    /**
+     * Retrieves a random enemy player from the game state.
+     *
+     * @param p_player The player for which an enemy is to be selected.
+     * @param p_gameState The current game state.
+     * @return A randomly selected enemy player.
+     */
     private Player getRandomEnemyPlayer(Player p_player, GameState p_gameState) {
         ArrayList<Player> l_playerList = new ArrayList<Player>();
         Random l_random = new Random();
@@ -155,17 +209,35 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
         return l_playerList.get(l_random.nextInt(l_playerList.size()));
     }
 
+    /**
+     * Retrieves the behavior of the player.
+     *
+     * @return The behavior of the player as a string ("Aggressive").
+     */
     @Override
     public String getPlayerBehavior() {
         return "Aggressive";
     }
 
+    /**
+     * Finds the strongest country owned by the player.
+     *
+     * @param p_player The player for which the strongest country is to be found.
+     * @param p_gameState The current game state.
+     * @return The strongest country owned by the player.
+     */
     public Country getStrongestCountry(Player p_player, GameState p_gameState) {
         List<Country> l_countriesOwnedByPlayer = p_player.getD_coutriesOwned();
         Country l_Country = calculateStrongestCountry(l_countriesOwnedByPlayer);
         return l_Country;
     }
 
+    /**
+     * Calculates the strongest country from a list of countries.
+     *
+     * @param l_listOfCountries The list of countries to evaluate.
+     * @return The strongest country from the list.
+     */
     public Country calculateStrongestCountry(List<Country> l_listOfCountries) {
         LinkedHashMap<Country, Integer> l_CountryWithArmies = new LinkedHashMap<Country, Integer>();
 
@@ -184,6 +256,12 @@ public class AggressivePlayer extends PlayerBehaviorStrategy {
 
     }
 
+    /**
+     * Checks if the player has deployed any armies.
+     *
+     * @param p_player The player to check.
+     * @return True if the player has deployed armies, otherwise false.
+     */
     private Boolean checkIfArmiesDepoyed(Player p_player){
         if(p_player.getD_coutriesOwned().stream().anyMatch(l_country -> l_country.getD_armies()>0)){
             return true;
--- src/main/Java/Models/BenevolentPlayer.java
+++ src/main/Java/Models/BenevolentPlayer.java
@@ -9,10 +9,22 @@ import java.util.Random;
 
 import Utils.CommonUtil;
 
+/**
+ * BenevolentPlayer class represents a player behavior strategy that focuses
+ * on helping weaker countries and avoiding aggressive moves.
+ */
 public class BenevolentPlayer extends PlayerBehaviorStrategy {
 
+    /** List of countries to deploy armies */
     ArrayList<Country> d_deployCountries = new ArrayList<Country>();
 
+    /**
+     * Creates an order for the player based on benevolent strategy.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created order as a string.
+     */
     @Override
     public String createOrder(Player p_player, GameState p_gameState) {
         System.out.println("Creating order for : " + p_player.getPlayerName());
@@ -66,6 +78,13 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
         return l_command;
     }
 
+    /**
+     * Creates a deploy order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created deploy order as a string.
+     */
     @Override
     public String createDeployOrder(Player p_player, GameState p_gameState) {
         if (p_player.getD_noOfUnallocatedArmies()>0) {
@@ -84,6 +103,13 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
         }
     }
 
+    /**
+     * Creates an advance order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @return The created advance order as a string.
+     */
     @Override
     public String createAdvanceOrder(Player p_player, GameState p_gameState) {
         int l_armiesToSend;
@@ -109,6 +135,14 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
                 + " " + l_armiesToSend;
     }
 
+    /**
+     * Creates a card order for the player.
+     *
+     * @param p_player The player for which the order is to be created.
+     * @param p_gameState The current game state.
+     * @param p_cardName The name of the card to be used.
+     * @return The created card order as a string.
+     */
     @Override
     public String createCardOrder(Player p_player, GameState p_gameState, String p_cardName) {
         int l_armiesToSend;
@@ -136,22 +170,47 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
         return null;
     }
 
+    /**
+     * Retrieves the behavior of the player.
+     *
+     * @return The behavior of the player as a string ("Benevolent").
+     */
     @Override
     public String getPlayerBehavior() {
         return "Benevolent";
     }
 
+    /**
+     * Retrieves a random country from a list of countries.
+     *
+     * @param p_listOfCountries The list of countries to choose from.
+     * @return A randomly selected country.
+     */
     private Country getRandomCountry(List<Country> p_listOfCountries) {
         Random l_random = new Random();
         return p_listOfCountries.get(l_random.nextInt(p_listOfCountries.size()));
     }
 
+    /**
+     * Finds the weakest country owned by the player.
+     *
+     * @param p_player The player for which the weakest country is to be found.
+     * @return The weakest country owned by the player.
+     */
     public Country getWeakestCountry(Player p_player) {
         List<Country> l_countriesOwnedByPlayer = p_player.getD_coutriesOwned();
         Country l_Country = calculateWeakestCountry(l_countriesOwnedByPlayer);
         return l_Country;
     }
 
+    /**
+     * Finds the weakest neighbor of a given country.
+     *
+     * @param l_randomSourceCountry The country for which the weakest neighbor is to be found.
+     * @param p_gameState The current game state.
+     * @param p_player The player owning the country.
+     * @return The weakest neighbor of the country.
+     */
     public Country getWeakestNeighbor(Country l_randomSourceCountry, GameState p_gameState, Player p_player) {
         List<Integer> l_adjacentCountryIds = l_randomSourceCountry.getD_adjacentCountryIds();
         List<Country> l_listOfNeighbors = new ArrayList<Country>();
@@ -167,6 +226,12 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
         return null;
     }
 
+    /**
+     * Calculates the weakest country from a list of countries.
+     *
+     * @param l_listOfCountries The list of countries to evaluate.
+     * @return The weakest country from the list.
+     */
     public Country calculateWeakestCountry(List<Country> l_listOfCountries) {
         LinkedHashMap<Country, Integer> l_CountryWithArmies = new LinkedHashMap<Country, Integer>();
 
@@ -186,6 +251,13 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
 
     }
 
+    /**
+     * Retrieves a random enemy player from the game state.
+     *
+     * @param p_player The player for which an enemy is to be selected.
+     * @param p_gameState The current game state.
+     * @return A randomly selected enemy player.
+     */
     private Player getRandomEnemyPlayer(Player p_player, GameState p_gameState) {
         ArrayList<Player> l_playerList = new ArrayList<Player>();
         Random l_random = new Random();
@@ -197,6 +269,12 @@ public class BenevolentPlayer extends PlayerBehaviorStrategy {
         return l_playerList.get(l_random.nextInt(l_playerList.size()));
     }
 
+    /**
+     * Checks if the player has deployed any armies.
+     *
+     * @param p_player The player to check.
+     * @return True if the player has deployed armies, otherwise false.
+     */
     private Boolean checkIfArmiesDepoyed(Player p_player){
         if(p_player.getD_coutriesOwned().stream().anyMatch(l_country -> l_country.getD_armies()>0)){
             return true;
