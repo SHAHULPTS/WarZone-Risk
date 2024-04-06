c0270a3f8b28251e982688c1fdc5637530d0b86e
Jayanth Apagundi <apagundijayanth1999@gmail.com>
2024-04-06 13:49:09 -0400

    OEP done- Game Services to do

--- src/main/Java/Models/OrderExecutionPhase.java
+++ src/main/Java/Models/OrderExecutionPhase.java
@@ -1,99 +1,110 @@
 package Models;
 
-import Controllers.GameEngine;
-import Exceptions.InvalidCommand;
-import Exceptions.InvalidMap;
-import Utils.Command;
-import Utils.CommonUtil;
-import Views.MapView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+import Constants.ApplicationConstants;
+import Controllers.GameEngine;
+import Exceptions.InvalidCommand;
+import Exceptions.InvalidMap;
+import Services.GameService;
+import Utils.Command;
+import Utils.CommonUtil;
+import Utils.ExceptionLogHandler;
+import Views.MapView;
 
-/**
- * Represents the phase where orders are executed in the game.
- * Extends Phase class.
- */
 public class OrderExecutionPhase extends Phase {
 
-    /**
-     * Constructs an OrderExecutionPhase object with the specified game engine and game state.
-     *
-     * @param p_gameEngine The game engine associated with this phase.
-     * @param p_gameState  The game state associated with this phase.
-     */
     public OrderExecutionPhase(GameEngine p_gameEngine, GameState p_gameState) {
         super(p_gameEngine, p_gameState);
     }
 
-    /**
-     * Performs card handling operation.
-     *
-     * @param p_enteredCommand The entered command.
-     * @param p_player         The player who entered the command.
-     * @throws IOException If an I/O error occurs.
-     */
+
+    @Override
+    protected void performLoadGame(Command p_command, Player p_player) throws InvalidCommand, InvalidMap, IOException {
+        printInvalidCommandInState();
+    }
+
+    @Override
+    protected void performSaveGame(Command p_command, Player p_player) throws InvalidCommand, InvalidMap, IOException {
+        List<java.util.Map<String, String>> l_operations_list = p_command.getOperationsAndArguments();
+
+        Thread.setDefaultUncaughtExceptionHandler(new ExceptionLogHandler(d_gameState));
+
+        if (l_operations_list == null || l_operations_list.isEmpty()) {
+            throw new InvalidCommand(ApplicationConstants.INVALID_COMMAND_ERROR_SAVEGAME);
+        }
+
+        for (Map<String, String> l_map : l_operations_list) {
+            if (p_command.checkRequiredKeysPresent(ApplicationConstants.ARGUMENTS, l_map)) {
+                String l_filename = l_map.get(ApplicationConstants.ARGUMENTS);
+                GameService.saveGame(this, l_filename);
+                d_gameEngine.setD_gameEngineLog("Game Saved Successfully to "+l_filename, "effect");
+            } else {
+                throw new InvalidCommand(ApplicationConstants.INVALID_COMMAND_ERROR_SAVEGAME);
+            }
+        }
+    }
+
     @Override
     protected void performCardHandle(String p_enteredCommand, Player p_player) throws IOException {
         printInvalidCommandInState();
     }
-    /**
-     * Performs advance operation.
-     *
-     * @param p_command The command entered by the player.
-     * @param p_player  The player who entered the command.
-     */
 
     @Override
     protected void performAdvance(String p_command, Player p_player) {
         printInvalidCommandInState();
     }
 
-    /**
-     * Initializes the order execution phase.
-     */
     @Override
-    public void initPhase() {
-        while (d_gameEngine.getD_CurrentPhase() instanceof OrderExecutionPhase) {
-            executeOrders();
+    public void initPhase(boolean isTournamentMode) {
+        executeOrders();
 
-            MapView l_map_view = new MapView(d_gameState);
-            l_map_view.showMap();
+        MapView l_map_view = new MapView(d_gameState);
+        l_map_view.showMap();
 
-            if (this.checkEndOftheGame(d_gameState))
-                break;
-
-            while (!CommonUtil.isCollectionEmpty(d_gameState.getD_players())) {
-                System.out.println("Press Y/y if you want to continue for next turn or else press N/n");
-                BufferedReader l_reader = new BufferedReader(new InputStreamReader(System.in));
+        if (this.checkEndOftheGame(d_gameState))
+            return;
 
 
-                try {
-                    String l_continue = l_reader.readLine().toLowerCase();;
+        try {
+            String l_continue = this.continueForNextTurn(isTournamentMode);
+            if (l_continue.equalsIgnoreCase("N") && isTournamentMode) {
+                d_gameEngine.setD_gameEngineLog("Start Up Phase", "phase");
+                d_gameEngine.setD_CurrentPhase(new StartUpPhase(d_gameEngine, d_gameState));
+            } else if (l_continue.equalsIgnoreCase("N") && !isTournamentMode) {
+                d_gameEngine.setStartUpPhase();
 
-                    switch (l_continue) {
-                        case "n":
-                            break;
-                        case "y":
-                            d_playerService.assignArmies(d_gameState);
-                            d_gameEngine.setIssueOrderPhase();
-                            break;
-                        default:
-                            System.out.println("Invalid Input");
-                            break;
-                    }
-                } catch (IOException l_e) {
-                    System.out.println("Invalid Input");
-                }
+            } else if (l_continue.equalsIgnoreCase("Y")) {
+                System.out.println("\n" + d_gameState.getD_numberOfTurnsLeft() + " Turns are left for this game. Continuing for next Turn.\n");
+                d_playerService.assignArmies(d_gameState);
+                d_gameEngine.setIssueOrderPhase(isTournamentMode);
+            } else {
+                System.out.println("Invalid Input");
             }
+        }  catch (IOException l_e) {
+            System.out.println("Invalid Input");
         }
+
     }
 
 
-    /**
-     * Executes the orders of the players.
-     */
+    private String continueForNextTurn(boolean isTournamentMode) throws IOException {
+        String l_continue = new String();
+        if (isTournamentMode) {
+            d_gameState.setD_numberOfTurnsLeft(d_gameState.getD_numberOfTurnsLeft() - 1);
+            l_continue = d_gameState.getD_numberOfTurnsLeft() == 0 ? "N" : "Y";
+        } else {
+            System.out.println("Press Y/y if you want to continue for next turn or else press N/n");
+            BufferedReader l_reader = new BufferedReader(new InputStreamReader(System.in));
+            l_continue = l_reader.readLine();
+        }
+        return l_continue;
+    }
+
+
     protected void executeOrders() {
         addNeutralPlayer(d_gameState);
         // Executing orders
@@ -111,16 +122,13 @@ public class OrderExecutionPhase extends Phase {
         d_playerService.resetPlayersFlag(d_gameState.getD_players());
     }
 
-    /**
-     * Adds a neutral player if not already present in the game state.
-     *
-     * @param p_gameState The game state where the neutral player needs to be added.
-     */
+
     public void addNeutralPlayer(GameState p_gameState) {
         Player l_player = p_gameState.getD_players().stream()
                 .filter(l_pl -> l_pl.getPlayerName().equalsIgnoreCase("Neutral")).findFirst().orElse(null);
         if (CommonUtil.isNull(l_player)) {
             Player l_neutralPlayer = new Player("Neutral");
+            l_neutralPlayer.setStrategy(new HumanPlayer());
             l_neutralPlayer.setD_moreOrders(false);
             p_gameState.getD_players().add(l_neutralPlayer);
         } else {
@@ -128,107 +136,78 @@ public class OrderExecutionPhase extends Phase {
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void performShowMap(Command p_command, Player p_player) {
-        MapView l_mview = new MapView(d_gameState);
-        l_mview.showMap();
+        MapView l_mapView = new MapView(d_gameState);
+        l_mapView.showMap();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void performCreateDeploy(String p_command, Player p_player) {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    protected void performAssignCountries(Command p_command, Player p_player) throws InvalidCommand, IOException {
+    protected void performAssignCountries(Command p_command, Player p_player, boolean isTournamentMode,
+                                          GameState p_gameState) throws InvalidCommand, IOException {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void createPlayers(Command p_command, Player p_player) throws InvalidCommand {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void performEditNeighbour(Command p_command, Player p_player)
             throws InvalidCommand, InvalidMap, IOException {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override
     protected void performEditCountry(Command p_command, Player p_player)
             throws InvalidCommand, InvalidMap, IOException {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+
+    @Override
     protected void performValidateMap(Command p_command, Player p_player) throws InvalidMap, InvalidCommand {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+
     @Override
     protected void performLoadMap(Command p_command, Player p_player) throws InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+
     @Override
     protected void performSaveMap(Command p_command, Player p_player) throws InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+
     @Override
     protected void performEditContinent(Command p_command, Player p_player)
             throws IOException, InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-    /**
-     * {@inheritDoc}
-     */
+
     @Override
     protected void performMapEdit(Command p_command, Player p_player) throws IOException, InvalidCommand, InvalidMap {
         printInvalidCommandInState();
     }
 
-    /**
-     * Checks if the game has reached its end condition.
-     *
-     * @param p_gameState The game state to check.
-     * @return True if the game has ended, false otherwise.
-     */
+
     protected Boolean checkEndOftheGame(GameState p_gameState) {
-        Integer l_countryCount = p_gameState.getD_map().getD_countries().size();
+        Integer l_totalCountries = p_gameState.getD_map().getD_countries().size();
+        d_playerService.updatePlayersInGame(p_gameState);
         for (Player l_player : p_gameState.getD_players()) {
-            if (l_player.getD_coutriesOwned().size() == l_countryCount) {
+            if (l_player.getD_coutriesOwned().size() == l_totalCountries) {
+                d_gameState.setD_winner(l_player);
                 d_gameEngine.setD_gameEngineLog("Player : " + l_player.getPlayerName()
                         + " has won the Game by conquering all countries. Exiting the Game .....", "end");
                 return true;
@@ -237,6 +216,10 @@ public class OrderExecutionPhase extends Phase {
         return false;
     }
 
-
-
+    @Override
+    protected void tournamentGamePlay(Command p_enteredCommand) {
+//		d_gameEngine.setD_gameEngineLog("\nStarting Execution Of Tournament Mode.....", "start");
+//		d_tournament.executeTournamentMode();
+//		d_tournament.printTournamentModeResult();
+    }
 }
